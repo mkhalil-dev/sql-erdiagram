610540161b2d130ee95798b1807427c132c3695f
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-11 12:43:36 +0300

    Added Query for list of students not enrolled in a course

--- sqlquery-excerse2.sql
+++ sqlquery-excerse2.sql
@@ -8,4 +8,8 @@ WHERE C.start_time = (SELECT MIN(C.start_time) FROM courses C);
 
 SELECT C.name
 FROM departments D, majorsIn MI, students S, enrolled E, courses C
-WHERE D.id = MI.depart_id AND S.id = MI.students_id AND S.id = E.students_id AND C.crn = E.courses_crn AND D.name = "BIF"; 
+WHERE D.id = MI.depart_id AND S.id = MI.students_id AND S.id = E.students_id AND C.crn = E.courses_crn AND D.name = "BIF";
+
+SELECT *
+FROM students S
+WHERE S.id NOT IN (SELECT E.students_id FROM enrolled E);
