6ecc70266f58feca859118f02e61c5babb5caa25
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-11 12:24:15 +0300

    Added Query for BIF Majors

--- sqlquery-excerse2.sql
+++ sqlquery-excerse2.sql
@@ -4,4 +4,8 @@ WHERE R.capacity >= 100;
 
 SELECT *
 FROM courses C
-WHERE C.start_time = (SELECT MIN(C.start_time) FROM courses C)
+WHERE C.start_time = (SELECT MIN(C.start_time) FROM courses C);
+
+SELECT C.name
+FROM departments D, majorsIn MI, students S, enrolled E, courses C
+WHERE D.id = MI.depart_id AND S.id = MI.students_id AND S.id = E.students_id AND C.crn = E.courses_crn AND D.name = "BIF"; 
